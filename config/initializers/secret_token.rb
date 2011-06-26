2a29591f82db4071b3ae587f48c5616f8feefe36
Alvaro Pereyra <alvaro@xendacentral.com>
2011-06-26 18:38:20 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoccoLa::Application.config.secret_token = 'd71372cc21f27371c11336284e0fded74e7eb8a2bd98728b857770bda809e72af4601e616a858057031b98dc1aa1825dc7e9330aacee59578fc59024bcf2f084'
