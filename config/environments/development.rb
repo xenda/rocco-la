ba64e8e5418d6df387666ab2b1aae60b8adf3e7c
Alvaro Pereyra <alvaro@xendacentral.com>
2011-06-26 19:42:03 -0500

    Adds styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,6 @@ RoccoLa::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  ENV['RAILS_ASSET_ID'] = ""
 end
 
