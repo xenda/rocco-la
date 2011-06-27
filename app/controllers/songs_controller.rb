667267e6cf1b4b0c690556c6de62dadb08dd9428
Alvaro Pereyra <alvaro@xendacentral.com>
2011-06-26 23:19:02 -0500

    Loads the lastest song from the queue and syncs accordingly

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,20 @@ class SongsController < InheritedResources::Base
   respond_to :json
   
   def current
-    render :json => {:video_id => "57tK6aQS_H0", :play_to => 0}
+    user_queue = UserQueue.last
+    unless user_queue.songs.empty?
+      current_time = Time.now
+      unless user_queue.current_song 
+        user_queue.current_song = user_queue.songs.last.youtube_id
+        user_queue.started_at = current_time 
+        user_queue.save
+      end
+      play_to = current_time - user_queue.started_at
+      video_id = user_queue.current_song
+      render :json => {:video_id => video_id, :play_to => play_to, :title => ""}
+    else
+      render :json => {:video_id => "57tK6aQS_H0", :play_to => 0, :title => ""}
+    end
   end
   
 end
--- app/models/user_queue.rb
+++ app/models/user_queue.rb
@@ -1,6 +1,8 @@
 class UserQueue
   include Mongoid::Document
 
+  field :current_song, :type => String
+  field :started_at, :type => DateTime
   belongs_to :user
   has_many :songs
 end
