62eded6940ff44281f46df8018264edba5aece8a
HPNeo <hpneo@hotmail.com>
2011-06-28 20:28:03 -0700

    commit antes de pull

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def facebook_like(url)
+    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(url)}&layout=standard&show_faces=true&width=150&action=like&font&colorscheme=light&height=30", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
+  end
+
 end
